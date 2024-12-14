ebbfb62c47c58edf8ee4f4abd1a1bdc939e6eddd
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2024-12-14 22:22:51 +0700

    scripts: add line rich menu setup

--- scripts/firstrun.ts
+++ scripts/firstrun.ts
@@ -2,7 +2,10 @@ import yoctoSpinner from "yocto-spinner"
 import prompts from "prompts"
 import chalk from "chalk"
 import axios from "axios"
+import fs from "fs"
 import "dotenv/config"
+
+const richMenuImagePath = './richmenu.png';
 
 (async () => {
     try {
@@ -27,6 +30,62 @@ import "dotenv/config"
         console.log("\n")
 
         if (!migrationPrompt.migrationPrompt) process.exit(0)
+
+        // set up rich menu
+        const richMenuCreationBody = {
+            size: {
+                width: 1200,
+                height: 405
+            },
+            selected: true,
+            name: 'main',
+            chatBarText: 'View your bills',
+            areas: [
+                {
+                    bounds: {
+                        x: 0,
+                        y: 0,
+                        width: 1200,
+                        height: 405
+                    },
+                    action: {
+                        type: 'uri',
+                        label: 'View your bills',
+                        uri: `${process.env.BASE_URL}/tenant`
+                    }
+                }
+            ]
+        }
+
+        const richMenuInitSpinner = yoctoSpinner({text: chalk.cyan(`Setting up LINE Rich Menu`)}).start()
+        const richMenuCreation = await axios.post('https://api.line.me/v2/bot/richmenu', richMenuCreationBody, { headers: { 'Content-Type': `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}` } })
+
+        if (richMenuCreation.status !== 200){
+            richMenuInitSpinner.error(chalk.red("LINE Rich Menu Creation failed!"))
+            process.exit(1)
+        }
+
+        const richMenuId = richMenuCreation.data['richMenuId']
+
+        const richMenuImageUpload = await axios.post(`https://api-data.line.me/v2/bot/richmenu/${richMenuId}/content`, fs.createReadStream(richMenuImagePath), {
+            headers: {
+              Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+              'Content-Type': 'image/png',
+            },
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity,
+            validateStatus() {
+                return true   
+            },
+        })
+
+        if (richMenuImageUpload.status !== 200){
+            richMenuInitSpinner.error(chalk.red("LINE Rich Menu Image upload failed!"))
+            process.exit(1)
+        }
+
+        richMenuInitSpinner.success(chalk.green("LINE Rich menu setup successfully!"))
+
         
         const alreadySetUpRes = await axios.get(`${process.env.BASE_URL}/api/setup`)
 
@@ -51,7 +110,11 @@ import "dotenv/config"
 
         const postReqSpinner = yoctoSpinner({text: chalk.cyan(`Setting up your account`)}).start()
 
-        const postRes = await axios.post(`${process.env.BASE_URL}/api/setup`, { osvtk: process.env.OSV_TOKEN, em: credentialPrompts.email, pw: credentialPrompts.password })
+        const postRes = await axios.post(`${process.env.BASE_URL}/api/setup`, { osvtk: process.env.OSV_TOKEN, em: credentialPrompts.email, pw: credentialPrompts.password }, { 
+            validateStatus() {
+                return true   
+            }
+        })
 
         if (postRes.status === 201){
             postReqSpinner.success("Your admin account has been set up!")
